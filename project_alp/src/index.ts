98b9dc31511e6c54013a6a8d9f3d068b81ede0bc
강민준 <back582cool@naver.com>
2021-01-04 16:33:46 +0900

    Update index.ts

--- project_alp/src/index.ts
+++ project_alp/src/index.ts
@@ -2,19 +2,27 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 
+//server parameter
 const app = new Koa();
 const router = new Router();
 const port: number = 4000;
 
-//라우터를 사용하기 전에 연결 해줘야 
+//server body
 app.use(bodyParser());
 
 router.get('(/*)', async (ctx) => {
-    ctx.body = '<h1>Hello World! <br><br>minjun_alpa!</h1>';
+    ctx.body = '<h1>Hello World! <br> minjun_alpa! <br> 안녕하세요@@@</h1>';
 });
 
+router.get('(/sub)', async (ctx) => {
+    ctx.body = '<h1>Hello World! <br> minjun_alpa! <br> sub sub sub 안녕하세요</h1>';
+});
+
+//server router
 app.use(router.routes());
 
 app.listen(port, ()=> {
-	console.log(`Koa server is listening on port ${port}`,`http://localhost:${port}/`);
+	console.log(`Koa server is listening on port ${port}`,`\t -> \thttp://localhost:${port}/`);
 });
+
+console.log('index gogo');
